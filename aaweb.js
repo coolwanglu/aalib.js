737bcc48cc27cbac81b46e0b01852e40c7060a2c
Lu Wang <coolwanglu@gmail.com>
2014-07-20 17:34:17 -0700

    ..

---
+++ aaweb.js
@@ -0,0 +1,16 @@
+mergeInto(LibraryManager.library, {
+    aaweb_init: function() {
+    },
+    aaweb_get_width: function() {
+        return 80;
+    },
+    aaweb_get_height: function() {
+        return 25;
+    },
+    aaweb_setattr: function(attr) {
+    },
+    aaweb_print: function(p) {
+    },
+    aaweb_gotoxy: function(x,y) {
+    }
+});
