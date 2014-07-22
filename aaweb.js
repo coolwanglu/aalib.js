02e720b5c3692de52c6dddcf8cb9de678d928d40
Lu Wang <coolwanglu@gmail.com>
2014-07-21 19:07:14 -0700

    font cache

--- aaweb.js
+++ aaweb.js
@@ -2,6 +2,7 @@ var LibraryAAWeb = {
   $aaweb: {
     canvas_node: null,
     ctx: null,
+    ctx_buffer: null,
     cols: 160,
     rows: 50,
     x: 0,
@@ -10,16 +11,25 @@ var LibraryAAWeb = {
     font: 'Source Code Pro',
     font_size: 8, 
     font_str: '',
+
+    normal_font_cache: null,
+    bold_font_cache: null,
+    dim_font_cache: null,
+    reverse_font_cache: null,
+    special_font_cache: null,
+    cur_font_cache: null,
+
     bg_color: '#000',
     dim_color: '#777',
     fg_color: '#fff',
+    special_color: '#f00',
 
     AA_NORMAL: 0,
     AA_DIM: 1,
     AA_BOLD: 2,
     AA_BOLDFONT: 3,
     AA_REVERSE: 4,
-    AA_SPECIAL: 5
+    AA_SPECIAL: 5,
   },
   aaweb_init: function() {
     var font_test_node = document.getElementById('aa-font-test');
@@ -27,18 +37,74 @@ var LibraryAAWeb = {
     font_test_node.innerHTML = 'm';
 
     var devicePixelRatio = window.devicePixelRatio;
-    aaweb.char_height = Math.max(1, font_test_node.clientHeight * devicePixelRatio);
-    aaweb.char_width = Math.max(1, font_test_node.clientWidth * devicePixelRatio);
+    var char_height = aaweb.char_height = Math.max(1, font_test_node.clientHeight * devicePixelRatio);
+    var char_width = aaweb.char_width = Math.max(1, font_test_node.clientWidth * devicePixelRatio);
 
     var canvas_node = aaweb.canvas_node = document.getElementById('aa-canvas');
     canvas_node.width = aaweb.cols * aaweb.char_width;
     canvas_node.height = aaweb.rows * aaweb.char_height;
     canvas_node.style.width = canvas_node.width / devicePixelRatio + canvas_node.offsetWidth - canvas_node.clientWidth + 'px';
     canvas_node.style.height = canvas_node.height / devicePixelRatio + canvas_node.offsetHeight - canvas_node.clientHeight + 'px';
+    aaweb.ctx = canvas_node.getContext('2d');
+    aaweb.ctx_buffer = aaweb.ctx.getImageData(0, 0, canvas_node.width, canvas_node.height);
 
-    var ctx = aaweb.ctx = canvas_node.getContext('2d');
-    ctx.font = aaweb.font_str = aaweb.font_size * devicePixelRatio + 'px "' + aaweb.font + '"';
+    // build font caches
+    var cache_canvas = document.createElement('canvas');
+    var canvas_height = 256 * char_height;
+    cache_canvas.width = char_width;
+    cache_canvas.height = canvas_height;
+    var ctx = cache_canvas.getContext('2d');
+    var font_str = aaweb.font_str = aaweb.font_size * devicePixelRatio + 'px "' + aaweb.font + '"';
+    
+    // normal text
+    ctx.fillStyle = aaweb.bg_color;
+    ctx.fillRect(0, 0, char_width, canvas_height);
+    ctx.fillStyle = aaweb.fg_color;
+    ctx.font = font_str;
     ctx.textBaseline = 'bottom';
+    for(var i = 0; i < 256; ++i) {
+      ctx.fillText(String.fromCharCode(i), 0, (i+1)*char_height, char_width);
+    }
+    aaweb.normal_font_cache = ctx.getImageData(0, 0, char_width, canvas_height).data;
+
+    // dim text
+    ctx.fillStyle = aaweb.bg_color;
+    ctx.fillRect(0, 0, char_width, canvas_height);
+    ctx.fillStyle = aaweb.dim_color;
+    for(var i = 0; i < 256; ++i) {
+      ctx.fillText(String.fromCharCode(i), 0, (i+1)*char_height, char_width);
+    }
+    aaweb.dim_font_cache = ctx.getImageData(0, 0, char_width, canvas_height).data;
+    
+    // special
+    ctx.fillStyle = aaweb.bg_color;
+    ctx.fillRect(0, 0, char_width, canvas_height);
+    ctx.fillStyle = aaweb.special_color;
+    for(var i = 0; i < 256; ++i) {
+      ctx.fillText(String.fromCharCode(i), 0, (i+1)*char_height, char_width);
+    }
+    aaweb.special_font_cache = ctx.getImageData(0, 0, char_width, canvas_height).data;
+
+    // reverse
+    ctx.fillStyle = aaweb.fg_color;
+    ctx.fillRect(0, 0, char_width, canvas_height);
+    ctx.fillStyle = aaweb.bg_color;
+    for(var i = 0; i < 256; ++i) {
+      ctx.fillText(String.fromCharCode(i), 0, (i+1)*char_height, char_width);
+    }
+    aaweb.reverse_font_cache = ctx.getImageData(0, 0, char_width, canvas_height).data;
+
+    // bold text
+    ctx.fillStyle = aaweb.bg_color;
+    ctx.fillRect(0, 0, char_width, canvas_height);
+    ctx.fillStyle = aaweb.fg_color;
+    ctx.font = 'bold ' + font_str;
+    for(var i = 0; i < 256; ++i) {
+      ctx.fillText(String.fromCharCode(i), 0, (i+1)*char_height, char_width);
+    }
+    aaweb.bold_font_cache = ctx.getImageData(0, 0, char_width, canvas_height).data;
+
+    aaweb.cur_font_cache = aaweb.normal_font_cache;
   },
   aaweb_build_font_data: function() {
     var char_width = aaweb.char_width;
@@ -91,41 +157,53 @@ var LibraryAAWeb = {
     return aaweb.rows;
   },
   aaweb_setattr: function(attr) {
-    aaweb.attr = attr;
-  },
-  aaweb_print: function(p) {
-    p = Pointer_stringify(p);
-    var x = aaweb.x * aaweb.char_width;
-    var y = aaweb.y * aaweb.char_height;
-    var w = p.length * aaweb.char_width;
-    var ctx = aaweb.ctx;
-    var bg_style = aaweb.bg_color;
-    var fg_style = aaweb.fg_color;
-    ctx.font = aaweb.font_str;
-    switch(aaweb.attr) {
+    switch(attr) {
       case aaweb.AA_NORMAL:
-      case aaweb.AA_SPECIAL:
       default:
+        aaweb.cur_font_cache = aaweb.normal_font_cache;
         break;
       case aaweb.AA_DIM:
-        fg_style = aaweb.dim_color;
+        aaweb.cur_font_cache = aaweb.dim_font_cache;
         break;
       case aaweb.AA_BOLD:
       case aaweb.AA_BOLDFONT:
-        ctx.font = 'bold ' + aaweb.font_str;
+        aaweb.cur_font_cache = aaweb.bold_font_cache;
         break;
       case aaweb.AA_REVERSE:
-        var tmp = fg_style;
-        fg_style = bg_style;
-        bg_style = tmp;
+        aaweb.cur_font_cache = aaweb.reverse_font_cache;
         break;
+      case aaweb.AA_SPECIAL:
+        aaweb.cur_font_cache = aaweb.special_font_cache;
+        break;
+    }  
+  },
+  aaweb_print: function(p) {
+    if(!p) return;
+    p = Pointer_stringify(p);
+    var char_width  = aaweb.char_width;
+    var char_height = aaweb.char_height;
+    var x = aaweb.x * char_width;
+    var y = aaweb.y * char_height;
+    var ctx_buffer_data = aaweb.ctx_buffer.data;
+    var cur_font_cache = aaweb.cur_font_cache;
+    var src_off = 0;
+    var dest_off = 0;
+    var canvas_width = aaweb.canvas_node.width;
+    for(var i = 0, l = p.length; i < l; ++i) {
+      var c = p.charCodeAt(i);
+      src_off = char_height * char_width * c * 4;
+      for(var cy = 0; cy < char_height; ++cy) {
+        dest_off = ((y + cy) * canvas_width + x) * 4;
+        for(var cx = 0; cx < char_width * 4; ++cx) {
+          ctx_buffer_data[dest_off++] = cur_font_cache[src_off++];
+        } 
+      }
+      x += char_width;
     }
-        
-    ctx.fillStyle = bg_style;
-    ctx.fillRect(x, y, w, aaweb.char_height);
-    ctx.fillStyle = fg_style;
-    ctx.fillText(p, x, y + aaweb.char_height, w);
     aaweb.x += p.length;
+  },
+  aaweb_flush: function() {
+    aaweb.ctx.putImageData(aaweb.ctx_buffer, 0, 0);
   },
   aaweb_gotoxy: function(x,y) {
     aaweb.x = x;
